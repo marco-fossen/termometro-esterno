9753d3ea2ffea21ad45432793746fc5c978cebfc
marco-fossen <92363779+marco-fossen@users.noreply.github.com>
2021-10-14 13:50:06 +0200

    Aggiorna main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+let temperaturaEsterna = 0
+radio.setGroup(1)
 basic.forever(function () {
-	
+    temperaturaEsterna = input.temperature()
+    basic.showString("" + (temperaturaEsterna))
+    radio.sendNumber(temperaturaEsterna)
+    basic.pause(1000)
 })
